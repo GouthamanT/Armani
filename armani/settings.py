7c3954b16141a32e21a87bce8553869a562953c3
Gouthaman Thangamuthu <gouthamant@parkisolutions.com>
2023-10-05 10:36:49 +0530

    host allowed

--- armani/settings.py
+++ armani/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4c$hkwu9m!m#6s=9zdjydlg(1=l__f8pf=5+09ba_%%m+@+*l0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.67.63']
 
 
 # Application definition
